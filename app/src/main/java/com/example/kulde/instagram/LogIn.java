1f27e0cb7c55fcf4ee4afa25a8f67a1acd948eb9
FreddieMe <alfredchen@Alfreds-MacBook-Pro.local>
2018-09-08 19:39:20 +1000

    Merge branch 'master' of https://github.com/kuldeepsuhag/Instagram_COMP90018

--- app/src/main/java/com/example/kulde/instagram/LogIn.java
+++ app/src/main/java/com/example/kulde/instagram/LogIn.java
@@ -1,24 +1,34 @@
 package com.example.kulde.instagram;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.w3c.dom.Text;
 
-public class LogIn extends AppCompatActivity {
-    private Button btsignin;
-    private EditText emailid;
-    private EditText passwordet;
-    private String email;
-    private String password;
-    private TextView signuptext;
+
+public class LogIn extends AppCompatActivity implements View.OnClickListener {
+    private Button signin;
+    private EditText email;
+    private EditText password;
+    //private String email;
+    //private String password;
+    private TextView signup;
     private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +36,58 @@ public class LogIn extends AppCompatActivity {
         setContentView(R.layout.activity_log_in);
         firebaseAuth = FirebaseAuth.getInstance();
 
-        TextView signuptext = (TextView)this.findViewById(R.id.signuptext);
-        signuptext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LogIn.this,SignUp.class);
-                startActivity(intent);
-            }
-        });
+        if (firebaseAuth.getCurrentUser() != null) {
+            finish();
+            startActivity(new Intent(this, MainPage.class));
+
+        }
+        progressDialog = new ProgressDialog(this);
+        email = findViewById(R.id.emailid);
+        password = findViewById(R.id.passwordet);
+        signup = findViewById(R.id.signuptext);
+        signin = findViewById(R.id.btsignin);
+        signup.setOnClickListener(this);
+        signin.setOnClickListener(this);
+    }
+
+    //TextView signuptext = (TextView)this.findViewById(R.id.signuptext);
+    public void onClick(View v) {
+        if (v == signup) {
+            Intent intent = new Intent(LogIn.this, SignUp.class);
+            startActivity(intent);
+        }
+        if(v == signin){
+            userLogin();
+        }
+    }
+    private void userLogin(){
+        String Email = email.getText().toString().trim();
+        String Password = password.getText().toString().trim();
+
+        if(TextUtils.isEmpty(Email)){
+            Toast.makeText(this,"Please Enter a valid Mail ID",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(Password)){
+            Toast.makeText(this,"Please Enter a password",Toast.LENGTH_SHORT).show();
+        }
+        progressDialog.setMessage("Login.....");
+        progressDialog.show();
+        firebaseAuth.signInWithEmailAndPassword(Email,Password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if (task.isSuccessful()) {
+                            //Toast.makeText(this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), MainPage.class));
+                        }
+                        else{
+                            Toast.makeText(LogIn.this,"Wrong Email ID and Password",Toast.LENGTH_SHORT).show();
+                        }
+
+                    } });
     }
 }
--- app/src/main/java/com/example/kulde/instagram/SignUp.java
+++ app/src/main/java/com/example/kulde/instagram/SignUp.java
@@ -1,26 +1,88 @@
 package com.example.kulde.instagram;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import javax.security.auth.login.LoginException;
 
-public class SignUp extends AppCompatActivity {
-    TextView signintext;
+public class SignUp extends AppCompatActivity implements View.OnClickListener{
+    private TextView signintext;
+    private EditText email, username, password;
+    private Button register;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        TextView signin = (TextView)findViewById(R.id.singintv);
-        signin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SignUp.this,LogIn.class);
-                startActivity(intent);
-            }
-        });
+       // System.out.print("Hello");
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        username = (EditText)findViewById(R.id.Etusername);
+        email = (EditText)findViewById(R.id.Etemailid);
+        password=(EditText)findViewById(R.id.Etpassword);
+        register=(Button)findViewById(R.id.btsignup);
+        signintext = (TextView)findViewById(R.id.singintv);
+        signintext.setOnClickListener(this);
+        register.setOnClickListener(this);
     }
+    public void onClick(View v) {
+        if (v == signintext) {
+            Intent intent = new Intent(SignUp.this, LogIn.class);
+            startActivity(intent);
+        }
+        if(v == register){
+            userRegister();
+        }
+    }
+    private void userRegister(){
+        final String Email = email.getText().toString().trim();
+        String Username = username.getText().toString().trim();
+        String Password = password.getText().toString().trim();
+
+        if(TextUtils.isEmpty(Email)){
+            Toast.makeText(this,"Please Enter Email ID",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(Username)){
+            Toast.makeText(this,"Please Enter  Username",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(Password)){
+            Toast.makeText(this,"Please Enter Password",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("Registering User....");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(Email,Password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(SignUp.this,"Registered Successfully",Toast.LENGTH_SHORT).show();
+                            finish();
+                            startActivity(new Intent(getApplicationContext(),MainPage.class));
+                        }
+                    }
+                });
+    }
+
 }
