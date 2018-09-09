6cbd7684b11c9f6973430772d434a84558be8d24
THINKPAD\wei <wxq10@case.edu>
2018-09-09 12:54:59 +1000

    share, gallery and photo

---
+++ app/src/main/java/com/example/kulde/instagram/share/PhotoFragment.java
@@ -0,0 +1,26 @@
+package com.example.kulde.instagram.share;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.kulde.instagram.R;
+
+public class PhotoFragment extends Fragment {
+    private static final String TAG = "PhotoFragment";
+
+    @Nullable
+    @Override
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_photo,container,false);
+        Log.d(TAG,"onCreateView:started.");
+
+        return view;
+    }
+
+}
