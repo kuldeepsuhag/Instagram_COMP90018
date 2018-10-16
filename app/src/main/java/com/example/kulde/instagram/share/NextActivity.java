1b35c8582adcf9bff2c4e22eb27cbf14e56c35c1
FreddieMe <alfredchen@Alfreds-MacBook-Pro.local>
2018-10-16 23:12:41 +1100

    delete location toaster

--- app/src/main/java/com/example/kulde/instagram/share/NextActivity.java
+++ app/src/main/java/com/example/kulde/instagram/share/NextActivity.java
@@ -203,9 +203,7 @@ public class NextActivity extends AppCompatActivity {
         @Override
         public void onLocationChanged(Location loc) {
             geoText.setText("hide location");
-            Toast.makeText(getBaseContext(),"Location changed : Lat: " +
-                            loc.getLatitude()+ " Lng: " + loc.getLongitude(),
-                    Toast.LENGTH_SHORT).show();
+
             String longitude = "Longitude: " +loc.getLongitude();
             Log.v(TAG, longitude);
             String latitude = "Latitude: " +loc.getLatitude();
